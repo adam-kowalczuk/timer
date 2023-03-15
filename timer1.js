463c9052c37d05fcd45ab79d847ffc61e6d14478
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-14 21:38:02 -0700

    Wrapped conditionals and for-loop in a function, renamed variables for clarity.

--- timer1.js
+++ timer1.js
@@ -1,21 +1,22 @@
-const argv = process.argv.slice(2).sort((a, b) => a - b);
+const input = process.argv.slice(2).sort((a, b) => a - b);
 
-if (!argv.length) {
-  console.log("Please input a number");
-  return;
-}
-const numbers = argv.filter((item) => {
-  return !isNaN(Number(item)) && item >= 0;
-});
-if (!numbers.length) {
-  console.log("Input provided is not a number or is less than zero");
-}
-for (let i = 0; i < numbers.length; i++) {
-  const timer = Number(numbers[i]) * 1000;
-  setTimeout(() => {
-    process.stdout.write('\x07');
-    console.log(timer);
-  }, timer);
-
+const timer1 = function(input) {
+  if (!input.length) {
+    console.log("Please input a number");
+    return;
+  }
+  const timeDelay = input.filter((item) => {
+    return !isNaN(Number(item)) && item >= 0;
+  });
+  if (!timeDelay.length) {
+    console.log("Input provided is not a number or is less than zero");
+  }
+  for (let i = 0; i < timeDelay.length; i++) {
+    const timer = Number(timeDelay[i]) * 1000;
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      console.log(timer);
+    }, timer);
+  }
 };
 
