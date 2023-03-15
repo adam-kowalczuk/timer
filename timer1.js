359205052ec1608208e4ef2cad99a047372d0d71
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-14 22:08:42 -0700

    Found source of undefined: implicit return. Finished cleaning up function and added 'beep' message.

--- timer1.js
+++ timer1.js
@@ -1,24 +1,25 @@
 const input = process.argv.slice(2).sort((a, b) => a - b);
 
-const timer1 = function(input) {
+function timer1(input) {
   if (!input.length) {
     console.log("Please input a number");
     return;
   }
-  const timeDelay = input.filter((item) => {
+  const timeCounts = input.filter((item) => {
     return !isNaN(Number(item)) && item >= 0;
-  })
-  if (!timeDelay.length) {
+  });
+  if (!timeCounts.length) {
     console.log("Input provided is not a number or is less than zero");
     return;
   }
-  for (let i = 0; i < timeDelay.length; i++) {
-    const delay = Number(timeDelay[i]) * 1000;
+  for (let i = 0; i < timeCounts.length; i++) {
+    const delay = Number(timeCounts[i]) * 1000;
     setTimeout(() => {
       process.stdout.write('\x07');
-      console.log("delay:", `${delay / 1000} seconds`);
+      console.log("beep");
     }, delay);
   }
 };
 
-timer1(input); 
+console.log(timer1(input));
+
