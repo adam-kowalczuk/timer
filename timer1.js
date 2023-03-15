a8c945cb5f4f08b45589156b2397115a0e353200
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-14 22:15:04 -0700

    Removed console.log call.

--- timer1.js
+++ timer1.js
@@ -3,14 +3,14 @@ const input = process.argv.slice(2).sort((a, b) => a - b);
 function timer1(input) {
   if (!input.length) {
     console.log("Please input a number");
-    return;
+    return 
   }
   const timeCounts = input.filter((item) => {
     return !isNaN(Number(item)) && item >= 0;
   });
   if (!timeCounts.length) {
     console.log("Input provided is not a number or is less than zero");
-    return;
+    return 
   }
   for (let i = 0; i < timeCounts.length; i++) {
     const delay = Number(timeCounts[i]) * 1000;
@@ -21,5 +21,5 @@ function timer1(input) {
   }
 };
 
-console.log(timer1(input));
+timer1(input);
 
