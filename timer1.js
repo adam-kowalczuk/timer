0cbf390228ba8a1ce621fc02d3f14aa383d258df
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-14 21:19:35 -0700

    Removed some if-statements and added a filtered array of numbers.

--- timer1.js
+++ timer1.js
@@ -1,14 +1,18 @@
 const argv = process.argv.slice(2).sort((a, b) => a - b);
-if (!argv.includes("number")) {
-  return "No numbers present";
-};
 let timer = argv[0] * 1000;
 
+if (!argv.length){
+  console.log ("Please input a number");
+  return;
+}
+const numbers = argv.filter((item) => {
+  return !isNaN(Number(item)) && item >= 0;
+})
 for (let i = 0; i < argv.length; i++) {
   console.log(timer);
   setTimeout(() => {
     process.stdout.write('\x07');
   }, timer);
   timer += (argv[i + 1] - argv[i]) * 1000;
-}
+};
 
