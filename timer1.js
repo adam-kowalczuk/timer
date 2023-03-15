2d6f30d5de41aba27226c6fe06171e88a6b7f5b3
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-14 21:33:49 -0700

    Moved timer into for-loop, working on making a function.

--- timer1.js
+++ timer1.js
@@ -1,5 +1,4 @@
 const argv = process.argv.slice(2).sort((a, b) => a - b);
-let timer = argv[0] * 1000;
 
 if (!argv.length) {
   console.log("Please input a number");
@@ -12,10 +11,11 @@ if (!numbers.length) {
   console.log("Input provided is not a number or is less than zero");
 }
 for (let i = 0; i < numbers.length; i++) {
+  const timer = Number(numbers[i]) * 1000;
   setTimeout(() => {
     process.stdout.write('\x07');
     console.log(timer);
   }, timer);
-  timer += (numbers[i + 1] - numbers[i]) * 1000;
+
 };
 
